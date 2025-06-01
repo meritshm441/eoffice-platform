3d9f00a4ac2f85bdbeedbf70a7fea2c18c85e414
Meron Teshome <meritshm9@gmail.com>
2025-06-01 21:43:10 +0300

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Shared e-Office System',
+  description: 'shared e-Office is a web-based application designed to streamline office operations and enhance productivity.',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
